b2302c4bcf2794891d0b3c36a7d1ca4fcfa04d89
disaster37 <linuxworkgroup@hotmail.com>
2025-05-19 12:22:53 +0000

    feat: upgrade libs and test on Opensearch 2.19.2

--- ci/dagger/main.go
+++ ci/dagger/main.go
@@ -24,7 +24,7 @@ import (
 )
 
 const (
-	OpensearchVersion string = "2.18.0"
+	OpensearchVersion string = "2.19.2"
 	username          string = "admin"
 	password          string = "vLPeJYa8.3RqtZCcAK6jNz"
 	mockgenVersion           = "v0.3.0"
@@ -51,7 +51,7 @@ func New(
 
 	return &OpensearchDashboard{
 		Src:          src,
-		GolangModule: dag.Golang(src),
+		GolangModule: dag.Golang(src.WithoutDirectory("ci")),
 	}, nil
 }
 
@@ -66,14 +66,29 @@ func (h *OpensearchDashboard) Ci(
 	// +optional
 	codeCoveToken *dagger.Secret,
 
+	// The git branch where you should to push
+	// You need to provide it when you are on PullRequest or on Tag
+	// +optional
+	gitBranch string,
+
+	// Set true if current build is a tag
+	// It will use the stable and alpha channel
+	// alpha channel only instead
+	// +optional
+	isTag bool,
+
 	// The git token
 	// +optional
 	gitToken *dagger.Secret,
 ) (dir *dagger.Directory, err error) {
 	var stdout string
 
+	h.GolangModule = dag.Golang(h.Src.WithoutDirectory("ci"), dagger.GolangOpts{Base: h.GolangModule.Container().WithExec([]string{"go", "mod", "tidy"})})
+
 	// Build
-	h.Build(ctx)
+	if _, err = h.Build(ctx).Sync(ctx); err != nil {
+		return nil, errors.Wrap(err, "Error when build project")
+	}
 
 	// Lint code
 	stdout, err = h.Lint(ctx)
@@ -100,7 +115,25 @@ func (h *OpensearchDashboard) Ci(
 			return nil, errors.Wrapf(err, "Error when upload report on CodeCov: %s", stdout)
 		}
 
-		if _, err = dag.Git().SetConfig(gitUsername, gitEmail, dagger.GitSetConfigOpts{BaseRepoURL: "github.com", Token: gitToken}).SetRepo(dir, dagger.GitSetRepoOpts{Branch: defaultGitBranch}).CommitAndPush(ctx, "Commit from CI. skip ci"); err != nil {
+		git := dag.GitModule(dir.WithDirectory("ci", h.Src.Directory("ci")), dagger.GitModuleOpts{Ci: "github"}).
+			SetConfig(dagger.GitModuleSetConfigOpts{
+				Username: gitUsername,
+				Email:    gitEmail,
+			})
+
+		if isTag {
+			gitBranch = defaultGitBranch
+		}
+
+		if _, err = git.CommitAndPush(
+			ctx,
+			gitToken,
+			dagger.GitModuleCommitAndPushOpts{
+				BranchName: gitBranch,
+				GitRepoURL: "https://github.com/disaster37/opensearch.git",
+				Message:    "Commit from CI",
+			},
+		); err != nil {
 			return nil, errors.Wrap(err, "Error when commit and push files change")
 		}
 	}
@@ -187,8 +220,7 @@ func (h *OpensearchDashboard) CodeCov(
 		src,
 		token,
 		dagger.CodecovUploadOpts{
-			Files:   []string{"coverage.out"},
-			Verbose: true,
+			Files: []string{"coverage.out"},
 		},
 	)
 }
--- go.mod
+++ go.mod
@@ -1,34 +1,33 @@
 module github.com/disaster37/opensearch-dashboard/v2
 
-go 1.23
+go 1.24
 
 require (
-	github.com/disaster37/opensearch/v2 v2.0.12
-	github.com/go-resty/resty/v2 v2.15.3
+	github.com/disaster37/opensearch/v2 v2.0.13
+	github.com/go-resty/resty/v2 v2.16.5
 	github.com/sirupsen/logrus v1.9.3
-	github.com/stretchr/testify v1.9.0
+	github.com/stretchr/testify v1.10.0
 	github.com/x-cray/logrus-prefixed-formatter v0.5.2
-	go.uber.org/mock v0.5.0
-	k8s.io/utils v0.0.0-20241104163129-6fe5fd82f078
+	go.uber.org/mock v0.5.2
+	k8s.io/utils v0.0.0-20250502105355-0f33e8f1c979
 )
 
 require (
 	emperror.dev/errors v0.8.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.31.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/net v0.27.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
-	golang.org/x/term v0.22.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
